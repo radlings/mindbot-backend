fb9743e5e750f91205b0da9896091a0686acc8cf
Stefan Su <stfn.su@live.de>
2019-11-01 18:06:31 +0100

    Write quotes to Firestore

--- writeQuote.py
+++ writeQuote.py
@@ -1,20 +1,29 @@
-import pyrebase
-from config import firebase_config
+from google.cloud import firestore
 
-firebase = pyrebase.initialize_app(firebase_config)
-db = firebase.database()
 
-# Dont Run. Data has already entered
+db = firestore.Client()
 
-# quote = {"text": "Believe in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy.", "author": "Norman Vincent Peale"}
-# db.child("quotes").push(quote)
+coll_ref = db.collection(u'quotes')
+coll_ref.add({
+    u"text": u"Believe in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy.", 
+    u"author": u"Norman Vincent Peale"
+    })
 
-# quotes = ["Your focus determines your reality.", "Do. Or do not. There is no try.", "In my experience there is no such thing as luck.", "Your eyes can deceive you. Don’t trust them.", "The Force will be with you. Always.", "There’s always a bigger fish.", "You can’t stop the change, any more than you can stop the suns from setting.", "Fear is the path to the dark side. Fear leads to anger; anger leads to hate; hate leads to suffering. I sense much fear in you.", "I’m one with the Force. The Force is with me."]
-# author = 'Star Wars'
+quotes = [
+    u"Your focus determines your reality.", "Do. Or do not. There is no try.",
+    u"In my experience there is no such thing as luck.",
+    u"Your eyes can deceive you. Don’t trust them.",
+    u"The Force will be with you. Always.",
+    u"There’s always a bigger fish.",
+    u"You can’t stop the change, any more than you can stop the suns from setting.",
+    u"Fear is the path to the dark side. Fear leads to anger; anger leads to hate; hate leads uto suffering. I sense much fear in you.",
+    u"I’m one with the Force. The Force is with me."
+    ]
+author = u"Star Wars"
 
-# for q in quotes:
-#     quote = {
-#         "text": q,
-#         "author": author
-#     }
-#     db.child("quotes").push(quote)
+for q in quotes:
+    quote = {
+        "text": q,
+        "author": author
+    }
+    coll_ref.add(quote)
